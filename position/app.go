1882bcd7eb52b9bcd8387d33c08fa82a3c182149
Toby Sullivan <tobyjsullivan@gmail.com>
2018-02-07 22:24:22 +1100

    Make min split configurable

--- position/app.go
+++ position/app.go
@@ -6,11 +6,11 @@ import (
 	"github.com/tobyjsullivan/shifty/qryptos"
 	"os"
 	"time"
+	"strconv"
 )
 
 const (
 	capitalAmount = 0.01
-	minimumSplit = 1.01
 	loopDelay     = 10 * time.Second
 	minimalUnit = 0.00000001
 )
@@ -20,12 +20,24 @@ var (
 	apiSecretKey  = os.Getenv("QRYPTOS_API_SECRET_KEY")
 	baseCurrency  = os.Getenv("POSITION_BASE_CURRENCY")
 	quoteCurrency = os.Getenv("POSITION_QUOTE_CURRENCY")
+	minimumSplit = 1.01
 
 	client = &qryptos.PrivateClient{
 		ApiTokenID:   apiTokenId,
 		ApiSecretKey: apiSecretKey,
 	}
 )
+
+func init() {
+	minSplitVar := os.Getenv("MIN_SPLIT")
+	if minSplitVar != "" {
+		var err error
+		minimumSplit, err = strconv.ParseFloat(minSplitVar, 64)
+		if err != nil {
+			panic("Error parsing MIN_SPLIT: "+err.Error())
+		}
+	}
+}
 
 func main() {
 	fmt.Println("[main] Running with token ID:", apiTokenId)
